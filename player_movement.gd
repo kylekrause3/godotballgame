44fdc25220f1391fc099dc679c3f3736f395cbed
kylekrause3 <kkrause317@gmail.com>
2023-10-05 18:31:07 -0400

    fix movement system, add velo ui

--- player_movement.gd
+++ player_movement.gd
@@ -4,8 +4,6 @@ var forward : Vector3 = Vector3(1, 0, 0);
 
 var move_input : Vector2
 
-@export var acceleration : float = 5
-var accel_multiplier : float = 1.0
 @export var base_speed : float = 25
 @export var max_velocity : float = 50
 @export var jump_velocity : float = 10.0
@@ -16,7 +14,7 @@ var local_rotation : float = 0
 var velocity : Vector3 = Vector3(0, 0, 0)
 
 var is_on_floor : bool = false
-var friction_amount : float = 0.1
+var rolling_resistance : float = 0.3
 
 var applied_speed : float = base_speed
 
@@ -25,7 +23,8 @@ func _ready():
 	pass
 
 
-func _physics_process(delta):
+func _physics_process(_delta):	
+	var xz_velocity : float = abs(linear_velocity.x) + abs(linear_velocity.z) /2
 	var dir = Vector3()
 	#movement input
 	move_input = Input.get_vector("left","right","down","up")
@@ -37,29 +36,29 @@ func _physics_process(delta):
 		applied_speed = sprint_speed
 	
 	dir = dir.rotated(Vector3(0, 1, 0), deg_to_rad(local_rotation))
-	velocity = lerp(velocity,dir*applied_speed,acceleration*accel_multiplier*delta)
-	apply_central_force(velocity)
+	velocity = dir * applied_speed
+	if is_on_floor:
+		apply_central_force(velocity)
+	else:
+		apply_central_force(velocity / 3)
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor:
-		accel_multiplier = 0.1
 		is_on_floor = false
 		apply_central_impulse(Vector3.UP * jump_velocity)
+	
+	var display_velocity : String = "Velocity:\t" + str(int(xz_velocity * 10))
+	get_parent().get_node("Label").text = display_velocity
 
 
 func _integrate_forces(state):
 	# govern speed of object
-	if state.linear_velocity.length() > max_velocity:
+	if state.linear_velocity.x > max_velocity || state.linear_velocity.y > max_velocity:
 		state.linear_velocity = state.linear_velocity.normalized() * max_velocity
 	# i should actually look up how to do a proper friction system 
 	# apply force of friction
-	if state.linear_velocity.length() > 0:
-		state.linear_velocity -= state.linear_velocity.normalized() * friction_amount
-#	if is_on_floor:
-#		if state.linear_velocity.length() > 0.2:
-#			var friction : Vector3 = Vector3(friction_amount, friction_amount, friction_amount)
-#			apply_central_force(state.linear_velocity.normalized() * -1 * friction)
-#		else:
-#			state.linear_velocity *= 0
+	if state.linear_velocity.length() > 0.2 and is_on_floor:
+		var friction : Vector3 = Vector3.ONE * rolling_resistance * 9.8 * self.mass
+		apply_central_force(state.linear_velocity.normalized() * -1 * friction)
 
 
 func _input(event):
@@ -71,6 +70,7 @@ func teleport(pos : Vector3) -> void:
 	apply_central_impulse(-self.linear_velocity)
 	self.set_position(pos)
 	self.linear_velocity = Vector3.ZERO
+	self.angular_velocity = Vector3.ZERO
 
 
 func change_orientation(y_rot : float) -> void:
